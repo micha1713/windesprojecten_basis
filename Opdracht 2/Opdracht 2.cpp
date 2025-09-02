8ab4c93124bd39faaa4462219aa76992c7f76710
Micha <pepermicha@protonmail.com>
2025-09-02 14:59:47 +0200

    opdracht 1 naam update, en opdracht 2

---
+++ Opdracht 2/Opdracht 2.cpp
@@ -0,0 +1,28 @@
+// Opdracht 2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <stdio.h>
+
+int A, B, C;
+int optelling;
+int gemiddelde;
+
+int main()
+{
+    printf_s("voer 3 getallen in om het gemiddelde te berekenen");
+    scanf_s("%d %d %d", &A, &B, &C);
+    optelling = A + B + C;
+    gemiddelde = optelling / 3;
+        printf_s("Gemiddelde = %d\n", gemiddelde);
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
